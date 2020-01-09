525332aaa2b975dfc8190957cb210484dccd36b6
Mario Torres <mario291187@gmail.com>
2020-01-09 17:36:41 -0600

    API S2

--- routes/API/v1.js
+++ routes/API/v1.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const rp = require('request-promise');
+const endpoints = require('../../endpoints');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.json({
@@ -8,5 +11,41 @@ router.get('/', function(req, res, next) {
         "version": '1.0'
     });
 });
+
+router.post('/search', (req, res)=> {
+    const {body} = req;
+
+    console.log(endpoints);
+
+    getToken(endpoints[0]).then(data => {
+console.log(data.access_token)
+    });
+
+    //Evalua la consulta
+
+    //Consulta fuentes
+
+    res.json({})
+});
+
+
+const getToken = endpoint => {
+    const opts = {
+        uri: endpoint.token_url,
+        method: 'POST',
+        contentType: 'x-www-form-urlencoded',
+        form: {
+            grant_type: 'password',
+            username: endpoint.username,
+            password: endpoint.password,
+            client_id: endpoint.client_id,
+            client_secret: endpoint.client_secret
+        },
+        json: true
+    };
+
+    return rp(opts);
+};
+
 
 module.exports = router;
