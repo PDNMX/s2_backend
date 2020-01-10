7662ab0769e463730b907452e18093053c0ba5a9
Mario Torres <mario291187@gmail.com>
2020-01-10 14:28:47 -0600

    S2 API

---
+++ routes/API/rest_data.js
@@ -0,0 +1,61 @@
+const rp = require('request-promise');
+
+const fetchData = (endpoint, options) => {
+    return getToken(endpoint).then(token_data => {
+        const {access_token} = token_data;
+
+        let opts = {
+            uri: endpoint.url,
+            method: 'POST',
+            qs: {
+                access_token: access_token
+            },
+            body: options,
+            json: true
+        };
+
+        //console.log(opts);
+
+        return rp(opts).then( data => data);
+    });
+};
+
+const fetchEntities = endpoint => {
+    return getToken(endpoint).then(token_data => {
+        const {access_token} = token_data;
+        const opts = {
+            uri: endpoint.entities_url,
+            method: 'GET',
+            qs: {
+                access_token: access_token,
+            },
+            json: true
+        };
+        //console.log(opts);
+
+        return rp(opts).then( entities => entities);
+    });
+};
+
+const getToken = endpoint => {
+    const opts = {
+        uri: endpoint.token_url,
+        method: 'POST',
+        contentType: 'x-www-form-urlencoded',
+        form: {
+            grant_type: 'password',
+            username: endpoint.username,
+            password: endpoint.password,
+            client_id: endpoint.client_id,
+            client_secret: endpoint.client_secret
+        },
+        json: true
+    };
+
+    return rp(opts);
+};
+
+module.exports = {
+    fetchData,
+    fetchEntities
+};
--- routes/API/v1.js
+++ routes/API/v1.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-const rp = require('request-promise');
 const endpoints = require('../../endpoints');
+const {fetchData, fetchEntities} = require('./rest_data');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -42,61 +42,5 @@ router.post('/entities', (req, res) => {
     });
 
 });
-
-
-const fetchData = (endpoint, options) => {
-    return getToken(endpoint).then(token_data => {
-        const {access_token} = token_data;
-
-        let opts = {
-            uri: endpoint.url,
-            method: 'POST',
-            qs: {
-                access_token: access_token
-            },
-            body: options,
-            json: true
-        };
-
-        //console.log(opts);
-
-        return rp(opts).then( data => data);
-    });
-};
-
-const fetchEntities = endpoint => {
-    return getToken(endpoint).then(token_data => {
-        const {access_token} = token_data;
-        const opts = {
-            uri: endpoint.entities_url,
-            method: 'GET',
-            qs: {
-                access_token: access_token,
-            },
-            json: true
-        };
-        //console.log(opts);
-
-        return rp(opts).then( entities => entities);
-    });
-};
-
-const getToken = endpoint => {
-    const opts = {
-        uri: endpoint.token_url,
-        method: 'POST',
-        contentType: 'x-www-form-urlencoded',
-        form: {
-            grant_type: 'password',
-            username: endpoint.username,
-            password: endpoint.password,
-            client_id: endpoint.client_id,
-            client_secret: endpoint.client_secret
-        },
-        json: true
-    };
-
-    return rp(opts);
-};
 
 module.exports = router;
