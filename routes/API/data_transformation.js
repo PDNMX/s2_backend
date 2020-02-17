7558206dbdb0ccda9e75b011c2d66827a288f8d6
Mario Torres <mario291187@gmail.com>
2020-02-17 16:22:28 -0600

    data transformations

--- routes/API/data_transformation.js
+++ routes/API/data_transformation.js
@@ -1,18 +1,15 @@
+const renameAttr = (object, a1, a2) => {
+    if (object.hasOwnProperty(a1)){
+        object[a2] = object[a1];
+        delete object[a1];
+    }
+};
 
 const sfp = data => {
 
-    const renameAttr = (object, a1, a2) => {
-        if (object.hasOwnProperty(a1)){
-            object[a2] = object[a1];
-            delete object[a1];
-        }
-    };
-
     data.results.forEach(d => {
-
         renameAttr(d, 'dependencia', 'institucionDependencia');
         renameAttr(d, 'tipo_procedimiento', 'tipoProcedimiento');
-
     });
 
     return data;
