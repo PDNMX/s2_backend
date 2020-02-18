643848353f6cc8bd10180274a45d8394933481f9
Mario Torres <mario291187@gmail.com>
2020-02-17 18:35:43 -0600

    data transformations -> codelists

--- routes/API/data_transformation.js
+++ routes/API/data_transformation.js
@@ -1,44 +1,11 @@
+const {procedimientos, nivelesResponsabilidad} = require('./code_lists.js');
+
 const renameAttr = (object, a1, a2) => {
     if (object.hasOwnProperty(a1)){
         object[a2] = object[a1];
         delete object[a1];
     }
 };
-
-const procedimientos = [
-    {
-        "clave": 1,
-        "valor": "CONTRATACIONES PÚBLICAS"
-    },
-    {
-        "clave": 2,
-        "valor": "CONCESIONES, LICENCIAS, PERMISOS, AUTORIZACIONES Y PRÓRROGAS"
-    },
-    {
-        "clave": 3,
-        "valor": "ENAJENACIÓN DE BIENES MUEBLES"
-    },
-    {
-        "clave": 4,
-        "valor": "ASIGNACIÓN Y EMISIÓN DE DICTÁMENES DE AVALÚOS NACIONALES"
-    }
-];
-
-const nivelesResponsabilidad = [
-    {
-        "clave": "A",
-        "valor": "ATENCIÓN"
-    },
-    {
-        "clave": "T",
-        "valor": "TRAMITACIÓN"
-    },
-    {
-        "clave": "R",
-        "valor": "RESOLUCIÓN"
-    }
-];
-
 
 const gql = data => {
     data.results.forEach(d => {
