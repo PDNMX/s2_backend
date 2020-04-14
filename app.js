f351e86f875289cb8d6c4c0c1c416524acb30ad2
Mario Torres <mario291187@gmail.com>
2020-04-14 16:56:15 -0500

    API v0

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var v0Router = require('./routes/API/v0');
 var apiRouter = require('./routes/API/v1');
 
 var app = express();
@@ -15,6 +16,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api/v0', v0Router);
 app.use('/api/v1', apiRouter);
 
 module.exports = app;
